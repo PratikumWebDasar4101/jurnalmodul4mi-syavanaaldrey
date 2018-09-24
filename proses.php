fcb456b9920bc2f75a8a82dacf6cb18d0ae09f48
syavanaaldrey <42725325+syavanaaldrey@users.noreply.github.com>
2018-09-24 10:58:44 +0700

    Add files via upload

--- proses.php
+++ proses.php
@@ -1,3 +1,19 @@
 <?php
 
+	$hobbi = $_POST['hobbi'];
+	
+	echo "hobbi Anda :";
+	foreach ($hobbi as $key => $value) {
+	echo "$value <br>";
+	}
+	print_r($hobbi);
+
+	$file = $_FILES['dokumen'];
+	$nama_file = $file['name'];
+	$nama_tmp = $file['tmp_name'];
+	$upload_dir = "upload/";
+	move_uploaded_file($nama_tmp,$upload_dir.$nama_file);
+	echo "File berhasil diunggah. <br>"
 ?>
+<img src="upload/<?php echo $nama_file; ?>">
+
